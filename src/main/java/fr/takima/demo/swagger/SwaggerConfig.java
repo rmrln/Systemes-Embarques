9f6dd6b52d28f64b046bdd4bd468764dcc714548
Mathilde Darras <mathilde.darras@gmail.com>
2019-12-12 11:31:35 +0100

    amelioration swaggerconfig.java securité, UI, erreur swagger

--- src/main/java/fr/takima/demo/swagger/SwaggerConfig.java
+++ src/main/java/fr/takima/demo/swagger/SwaggerConfig.java
@@ -1,29 +1,66 @@
 package fr.takima.demo.swagger;
 
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.AlternateTypeRules;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Autowired
+    private TypeResolver typeResolver;
+
     @Bean
     public Docket api() {
-        //System.out.println(PathSelectors.ant("/**"));
+        ResponseMessage responseMessage500 = new ResponseMessageBuilder()
+                .code(500)
+                .message("500 message")
+                .responseModel(new ModelRef("Error"))
+                .build();
+
+        ResponseMessage responseMessage403 = new ResponseMessageBuilder()
+                .code(403)
+                .message("Forbidden!")
+                .build();
+        ArrayList<ResponseMessage> responseMessageArrayList = new ArrayList<>();
+        responseMessageArrayList.add(responseMessage403);
+        responseMessageArrayList.add(responseMessage500);
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("fr.takima.demo"))
                 .paths(PathSelectors.ant("/api/**"))
-                .build().apiInfo(apiInfo()).useDefaultResponseMessages(false);
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(
+                        RequestMethod.GET,
+                        responseMessageArrayList)
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(typeResolver.resolve(DeferredResult.class,
+                                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                                typeResolver.resolve(WildcardType.class)));
 
     }
 
@@ -36,5 +73,38 @@ public class SwaggerConfig {
                 new Contact("EPF Roxane, Mathilde, Anto et Mika", "localhost:8080/", "mathilde.darras@epfedu.fr"), "License of API", "API license URL", Collections.emptyList());
 
         return apiInfo;
+    }
+
+    @Bean
+    SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()//<19>
+                .clientId("test-app-client-id")
+                .clientSecret("test-app-client-secret")
+                .realm("test-app-realm")
+                .appName("test-app")
+                .scopeSeparator(",")
+                .additionalQueryStringParams(null)
+                .useBasicAuthenticationWithAccessCodeGrant(false)
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()//<20>
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
     }
 }
