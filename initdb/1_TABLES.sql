ede445edc64eeb01ba5ab46b8df12ac65254ecd6
Mathilde Darras <mathilde.darras@gmail.com>
2020-01-07 12:25:41 +0100

    ajout script creation bdd

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -1,3 +1,20 @@
+create table temperatures
+    (
+        id bigint auto_increment,
+        constraint temperatures_pk
+        primary key (id),
+        date TEXT not null,
+        temperature double null
+    );
+
+create table positions
+(
+    id bigint auto_increment,
+    constraint positions_pk
+        primary key (id),
+    date TEXT not null,
+    position int null
+);
 
 create table respiration
 (
