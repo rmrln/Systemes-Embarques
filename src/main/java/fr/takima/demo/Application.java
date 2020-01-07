9e972696b4fed1c5ad1136db2c8e140e9b5b8408
Mathilde Darras <mathilde.darras@gmail.com>
2020-01-07 12:31:07 +0100

    suppression mauvais imports

--- src/main/java/fr/takima/demo/Application.java
+++ src/main/java/fr/takima/demo/Application.java
@@ -1,21 +1,11 @@
 package fr.takima.demo;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.takima.demo.model.Temperature;
-import fr.takima.demo.service.TemperatureService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.List;
 
 /**
  *
